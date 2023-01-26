a40f05e520ce4071376099008c5777726ad47fa8
Walesca <walesca.telemaq@gmail.com>
2023-01-26 15:01:16 +0100

    last-commit

--- js/script.js
+++ js/script.js
@@ -1,33 +1,25 @@
 let rightbt = document.getElementById('right');
-let leftbt = document.getElementById('left');
-let windowSlider = document.getElementById('window-slider').offsetWidth;
-let listSlide = document.getElementById('list-slide')
-let slide = document.getElementsByClassName('slide');
-let nbSlide = slide.length;
+let leftbt= document.getElementById('left');
+let listSlide = document.getElementById('list-slide');
 let mLeft = 0;
 
-
 rightbt.addEventListener('click', function () {
-   if (mLeft - windowSlider == windowSlider*-5) {
 
-      this.click.stopPropagation()
-  }
-  else {
-      mLeft -= windowSlider;
-      listSlide.style.marginLeft = mLeft + 'px';
-  }
+    if (mLeft - 500 >= -2500) {
+        mLeft -= 500;
+
+        listSlide.style.marginLeft = mLeft + 'px';
+    }
+
 });
 
 
-
 leftbt.addEventListener('click', function () {
-   if (mLeft + windowSlider == windowSlider+1) {
 
-      this.click.stopPropagation()
-  }
-  else {
-      mLeft += windowSlider;
-      listSlide.style.marginLeft = mLeft + 'px';
-  }
+    if (mLeft + 500 <=  0) {
+        mLeft += 500;
+
+        listSlide.style.marginLeft = mLeft + 'px';
+    } 
 
 });
