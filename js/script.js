13c3f6089b5f01f044440e97335f7a4620e7b1dc
Walesca <walesca.telemaq@gmail.com>
2023-01-26 15:25:23 +0100

    true last commit

--- js/script.js
+++ js/script.js
@@ -1,34 +1,9 @@
-<<<<<<< HEAD
 let rightbt = document.getElementById('right');
 let leftbt= document.getElementById('left');
 let listSlide = document.getElementById('list-slide');
 let mLeft = 0;
 
 rightbt.addEventListener('click', function () {
-=======
-let leftbt = document.getElementById('left');
-let rightbt = document.getElementById('right');
-let windowSlider = document.getElementById('window-slider').offsetWidth;
-let listSlide = document.getElementById('list-slide');
-let slide = document.getElementsByClassName('slide');
-let nbSlide = slide.length;
-let mLeft = 0;
-
-leftbt.addEventListener('click', function () {
-    if (mLeft + windowSlider*2 == windowSlider && windowSlider !== undefined) {
- 
-       this.click.stopPropagation()
-   }
-   else {
-       mLeft += windowSlider;
-       listSlide.style.marginLeft = mLeft + 'px';
-   }
- 
- });
-
-rightbt.addEventListener('click', function () {
-   if (mLeft - windowSlider == windowSlider) {
->>>>>>> 55d76c0240eb198fe63cb5e2b9aad57ce0e0aef4
 
     if (mLeft - 500 >= -2500) {
         mLeft -= 500;
@@ -39,7 +14,6 @@ rightbt.addEventListener('click', function () {
 });
 
 
-<<<<<<< HEAD
 leftbt.addEventListener('click', function () {
 
     if (mLeft + 500 <=  0) {
@@ -49,7 +23,3 @@ leftbt.addEventListener('click', function () {
     } 
 
 });
-=======
-
-
->>>>>>> 55d76c0240eb198fe63cb5e2b9aad57ce0e0aef4
